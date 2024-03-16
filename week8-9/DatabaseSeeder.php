3a5d899aff11156c6143f192d9d562e24651e8bc
Arnulfo Sanchez <arnulfo457@gmail.com>
2024-03-15 20:10:27 -0400

    in class assignemnt laravel

---
+++ week8-9/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Student;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory()->count(2)->create();
+        Student::factory()->count(20)->create();
+    }
+}
